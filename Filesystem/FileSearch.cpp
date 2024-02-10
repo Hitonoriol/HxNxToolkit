0958856e6e680c8e62ee86e646fb8725a221b1e7
Hitonoriol <hitonoriolx@gmail.com>
2024-02-11 00:58:54 +0200

    Add option to search file contents

--- Filesystem/FileSearch.cpp
+++ Filesystem/FileSearch.cpp
@@ -5,6 +5,8 @@
 #include <QRegularExpression>
 #include <QFileDialog>
 
+#include <locale>
+
 // TODO: maybe hide this behind macros for portability? if anybody even cares...
 #include <windows.h>
 #include <shlobj.h>
@@ -15,8 +17,10 @@ FileSearch::FileSearch(QWidget *parent)
 	ui.setupUi(this);
 	ui.WalkedFileslabel->setVisible(false);
 	ui.StopButton->setVisible(false);
+	OnNameSearchSelected();
 
 	connect(this, &FileSearch::FileFound, this, &FileSearch::OnFileFound);
+	connect(this, &FileSearch::ContentsFound, this, &FileSearch::OnContentsFound);
 	connect(this, &FileSearch::SearchFinished, this, &FileSearch::OnSearchFinished);
 	connect(this, &FileSearch::FilesWalked, this, &FileSearch::OnFilesWalked);
 }
@@ -68,11 +72,15 @@ void FileSearch::OnBrowsePress()
 
 void FileSearch::OnSearchPress()
 {
-	ui.ResultList->clear();
-
 	auto baseDirPath = ui.PathField->text();
 	auto searchPattern = ui.SearchPatternField->text();
-	FindFiles(baseDirPath, searchPattern);
+
+	if (ui.NameRadio->isChecked()) {
+		FindFiles(baseDirPath, searchPattern);
+	} else if (ui.ContentsRadio->isChecked()) {
+		FindContents(baseDirPath, searchPattern);
+	}
+
 	emit Modified(this);
 }
 
@@ -85,6 +93,25 @@ void FileSearch::OnFileFound(std::filesystem::path foundPath)
 {
 	ui.ResultList->addItem(QString::fromStdString(foundPath.string()));
 	emit Modified(this);
+}
+
+void FileSearch::OnContentsFound(std::filesystem::path foundPath, size_t line, QString context)
+{
+	static int a = 0;
+	OutputDebugStringA(("Preview(" + context.toStdString() + ")Preview\n").c_str());
+
+	auto newIdx = ui.ResultTable->rowCount();
+	ui.ResultTable->insertRow(newIdx);
+	ui.ResultTable->setItem(newIdx, 0, new QTableWidgetItem(QString::number(line)));
+	ui.ResultTable->setItem(newIdx, 1, new QTableWidgetItem(QString::fromStdString(foundPath.string())));
+	ui.ResultTable->setItem(newIdx, 2, new QTableWidgetItem(context));
+
+	for (size_t i = 0; i < 3; ++i) {
+		auto item = ui.ResultTable->item(newIdx, i);
+		item->setFlags(item->flags() & ~Qt::ItemIsEditable);
+	}
+
+	ui.ResultTable->resizeRowToContents(newIdx);
 }
 
 void FileSearch::OnSearchFinished()
@@ -100,35 +127,44 @@ void FileSearch::OnFilesWalked(size_t count)
 	ui.WalkedFileslabel->setText(QString("[walked %1 files]").arg(count));
 }
 
+void FileSearch::OnNameSearchSelected()
+{
+	ui.ResultList->setVisible(true);
+	ui.ResultTable->setVisible(false);
+}
+
+void FileSearch::OnContentsSearchSelected()
+{
+	ui.ResultList->setVisible(false);
+	ui.ResultTable->setVisible(true);
+}
+
 void FileSearch::OnSearchResultDoubleClicked(QListWidgetItem* item)
 {
-	auto pathStr = item->text().toStdWString();
+	auto pathStr = item->text();
+	OpenInExplorer(pathStr);
+}
 
-	// TODO: ...and this too, obviously
-	if (auto idList = ILCreateFromPath(pathStr.data())) {
-		SHOpenFolderAndSelectItems(idList, 0, nullptr, 0);
-		ILFree(idList);
-	}
+void FileSearch::OnSearchResultDoubleClicked(QTableWidgetItem* item)
+{
+	auto row = item->row();
+	auto pathStr = ui.ResultTable->item(row, 1)->text();
+	OpenInExplorer(pathStr);
 }
 
 void FileSearch::FindFiles(
 	const QString& baseDirPath,
 	const QString& searchPattern)
 {
-	if (searchThread.joinable()) {
-		searchThread.join();
-	}
-
-	ui.SearchButton->setEnabled(false);
-	ui.WalkedFileslabel->setVisible(true);
-	ui.StopButton->setVisible(true);
-
+	PrepareForSearch();
 	searchThread = std::thread([=] {
-		std::vector<std::filesystem::path> files;
 		std::filesystem::recursive_directory_iterator dirIt(baseDirPath.toStdString());
 		auto searchRegexStr = QRegularExpression::wildcardToRegularExpression(
 			searchPattern, QRegularExpression::UnanchoredWildcardConversion);
-		QRegularExpression searchRegex(searchRegexStr, QRegularExpression::CaseInsensitiveOption);
+		auto flags = ui.CaseSensitiveBox->isChecked()
+			? QRegularExpression::NoPatternOption
+			: QRegularExpression::CaseInsensitiveOption;
+		QRegularExpression searchRegex(searchRegexStr, flags);
 		size_t walkedFiles{};
 		for (auto& dirEntry : dirIt) {
 			if (stop) {
@@ -150,4 +186,86 @@ void FileSearch::FindFiles(
 
 		emit SearchFinished();
 	});
+}
+
+void FileSearch::FindContents(const QString& baseDirPath, const QString& searchPattern)
+{
+	PrepareForSearch();
+	searchThread = std::thread([=] {
+		std::filesystem::recursive_directory_iterator dirIt(baseDirPath.toStdString());
+		auto searchRegexStr = QRegularExpression::wildcardToRegularExpression(
+			searchPattern, QRegularExpression::UnanchoredWildcardConversion);
+		auto flags = ui.CaseSensitiveBox->isChecked()
+			? QRegularExpression::NoPatternOption
+			: QRegularExpression::CaseInsensitiveOption;
+		QRegularExpression searchRegex(searchRegexStr, flags);
+		size_t walkedFiles{};
+		for (auto& dirEntry : dirIt) {
+			if (stop) {
+				break;
+			}
+
+			if (dirEntry.is_directory()) {
+				continue;
+			}
+
+			auto path = dirEntry.path();
+			path.make_preferred();
+			QFile foundFile(path);
+			if (!foundFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+				continue;
+			}
+
+			std::locale locale;
+			QTextStream in(&foundFile);
+			size_t lineNumber{};
+			while (!in.atEnd()) {
+				++lineNumber;
+				auto line = in.readLine();
+				if (searchRegex.match(line).hasMatch()) {
+					static constexpr size_t MaxLength = 100;
+					line.truncate(MaxLength);
+					auto preview = line.toStdWString();
+					auto nonPrintableCharIt = std::find_if(preview.begin(), preview.end(), [&](wchar_t c) {
+						if (!std::isspace(c, locale) && !std::isgraph(c, locale))
+							return true;
+						return false;
+					});
+					if (nonPrintableCharIt != preview.end()) {
+						continue;
+					}
+
+					emit ContentsFound(path, lineNumber, line);
+				}
+			}
+
+			foundFile.close();
+			emit FilesWalked(++walkedFiles);
+		}
+
+		emit SearchFinished();
+	});
+}
+
+void FileSearch::PrepareForSearch()
+{
+	if (searchThread.joinable()) {
+		searchThread.join();
+	}
+
+	ui.SearchButton->setEnabled(false);
+	ui.WalkedFileslabel->setVisible(true);
+	ui.StopButton->setVisible(true);
+
+	ui.ResultList->clear();
+	ui.ResultTable->setRowCount(0);
+}
+
+void FileSearch::OpenInExplorer(const QString& pathStr)
+{
+	// TODO: ...and this too, obviously
+	if (auto idList = ILCreateFromPath(pathStr.toStdWString().data())) {
+		SHOpenFolderAndSelectItems(idList, 0, nullptr, 0);
+		ILFree(idList);
+	}
 }
--- Filesystem/FileSearch.h
+++ Filesystem/FileSearch.h
@@ -23,6 +23,7 @@ public:
 
 signals:
 	void FileFound(std::filesystem::path foundPath);
+	void ContentsFound(std::filesystem::path foundPath, size_t line, QString context);
 	void SearchFinished();
 	void FilesWalked(size_t count);
 
@@ -32,15 +33,28 @@ private slots:
 	void OnStopPress();
 
 	void OnFileFound(std::filesystem::path foundPath);
+	void OnContentsFound(std::filesystem::path foundPath, size_t line, QString context);
 	void OnSearchFinished();
 	void OnFilesWalked(size_t count);
 
+	void OnNameSearchSelected();
+	void OnContentsSearchSelected();
+
 	void OnSearchResultDoubleClicked(QListWidgetItem* item);
+	void OnSearchResultDoubleClicked(QTableWidgetItem* item);
 
 private:
 	void FindFiles(
 		const QString& baseDirPath,
 		const QString& searchPattern);
+
+	void FindContents(
+		const QString& baseDirPath,
+		const QString& searchPattern);
+
+	void PrepareForSearch();
+
+	void OpenInExplorer(const QString& pathStr);
 
 	Ui::FileSearchClass ui;
 	std::thread searchThread;
